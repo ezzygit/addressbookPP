50ac1c3ab2f67c58c8093c3e876e748db2a5654f
Ian Davenport <g.chatzichristodoulou@outlook.com>
2021-01-15 15:15:27 +0000

    final

--- src/App.js
+++ src/App.js
@@ -22,17 +22,20 @@ addNumbers = () => {
 
 render () {
     return (
-        <div>
-           <h1>Adress book </h1>
-              <input value={this.state.input} onChange={this.handleInput}></input>
-              
-              <button value= {this.state.input} onClick={(this.addNumbers)}>add</button>
-        
-           <div className="numbers">{this.state.numbers.map((num,index)=>{
-                 return <p key={index}>{num}</p>;})}
-           </div>
+        <div className="App">
+        <div className="App-container">
+        <h1 className="title">Address book </h1> 
+           <br></br>
 
+              <input className="input" value={this.state.input} onChange={this.handleInput}></input>
+              
+              <button className="button" value= {this.state.input} onClick={(this.addNumbers)}>add</button>
+        
+           <div className="numbers" >{this.state.numbers.map((num,index)=>{
+                 return <p className="numbers" key={index}>{num}</p>;})}
+           </div>
         </div>
+    </div>  
             
         
 )}}
